7fff0857611881529611c186079cb9246190a161
Jed <bibatjed@gmail.com>
2021-01-25 20:29:35 +0800

    chore: remove console.log

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 let isActive = false;
 
 $('.menu').on('click', function(){
-    console.log("aaa")
     if(isActive){
         $(this).removeClass('active');
         $('body').removeClass('menu-open')
